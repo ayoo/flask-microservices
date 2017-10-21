b7f97464c702c3385662bdf5f689e9803dec96c6
Andy Yoo <javasonix@gmail.com>
2017-10-21 22:50:01 +1300

    added more text examples for getting a single user

--- project/tests/test_users.py
+++ project/tests/test_users.py
@@ -15,6 +15,7 @@ class TestUserService(BaseTestCase):
         self.assertIn('pong!', data['message'])
         self.assertIn('success', data['status'])
 
+
     def test_add_user(self):
         with self.client:
             response = self.client.post(
@@ -98,3 +99,23 @@ class TestUserService(BaseTestCase):
             self.assertIn('tester', data['data']['username'])
             self.assertIn('tester@test.com', data['data']['email'])
             self.assertIn('success', data['status'])
+
+
+    def test_single_user_no_id(self):
+        """Ensure error is thrown if an id is not provided."""
+        with self.client:
+            response = self.client.get('/users/blah')
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 404)
+            self.assertIn('User does not exist', data['message'])
+            self.assertIn('fail', data['status'])
+
+
+    def test_single_user_incorrect_id(self):
+        """Ensure error is thrown if the id does not exist."""
+        with self.client:
+            response = self.client.get('/users/999')
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 404)
+            self.assertIn('User does not exist', data['message'])
+            self.assertIn('fail', data['status'])
